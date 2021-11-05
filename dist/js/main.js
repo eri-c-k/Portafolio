df0946c0728febc34e13caa7a7aa8f2aece1b0f6
erick <erick@debian.erick>
2021-11-04 18:09:39 -0700

    fixed broken links, changed navbar items

--- dist/js/main.js
+++ dist/js/main.js
@@ -118,8 +118,7 @@ const modals = {
     title: 'EdgeLedger Website',
     img: 1,
     id: 'modal-1',
-    text:
-      'It is a business Website template build on HTML, CSS and JQuery, the layout is mainly design with flexbox and the navigation bar smooth scrolling the to the landing page.',
+    text: 'It is a business Website template build on HTML, CSS and JQuery, the layout is mainly design with flexbox and the navigation bar smooth scrolling the to the landing page.',
     url: 'https://naughty-lumiere-69e1ba.netlify.app/',
     repo: 'https://github.com/Erick-Hdez/Edge-Ledger',
     icons: ` 
@@ -132,8 +131,7 @@ const modals = {
     title: 'Guess My Number',
     img: 6,
     id: 'modal-2',
-    text:
-      'Guess My Number is a simple game that generates random numbers between  1 to 20 where the player has to guess the randoom number and when it guessed the screen color turns to green. This game  is build on HTML, CSS and the logic with JavaScript.',
+    text: 'Guess My Number is a simple game that generates random numbers between  1 to 20 where the player has to guess the randoom number and when it guessed the screen color turns to green. This game  is build on HTML, CSS and the logic with JavaScript.',
     url: 'https://jolly-shaw-3d7118.netlify.app/',
     repo: 'https://github.com/Erick-Hdez/Guess-my-number',
     icons: `
@@ -146,9 +144,8 @@ const modals = {
     title: 'Naré Website',
     img: 2,
     id: 'modal-3',
-    text:
-      'It is a website that I designed for a client that sells delicious walnut and apple tarts in the city of Chihuahua Mexico. The web site is fully responsive and is built on HTML, CSS and JQuery.',
-    url: 'https://www.nare.mx/',
+    text: 'It is a website that I designed for a client that sells delicious walnut and apple tarts in the city of Chihuahua Mexico. The web site is fully responsive and is built on HTML, CSS and JQuery.',
+    url: 'https://dreamy-rosalind-7facea.netlify.app/',
     repo: 'https://github.com/Erick-Hdez/Nare',
     icons: `
       <i class="devicon-html5-plain-wordmark colored"></i>
@@ -161,8 +158,7 @@ const modals = {
     title: 'News Website',
     img: 3,
     id: 'modal-4',
-    text:
-      'It is a static website  template  build with grid CSS and HTML ready to adapt as a News Website or personal blog.',
+    text: 'It is a static website  template  build with grid CSS and HTML ready to adapt as a News Website or personal blog.',
     url: 'https://stoic-curie-a04cae.netlify.app/',
     repo: 'https://github.com/Erick-Hdez/NewsGrid',
     icons: `
@@ -174,10 +170,9 @@ const modals = {
     title: 'My Portfolio Website',
     img: 5,
     id: 'modal-5',
-    text:
-      'It is my personalized web, built on HTML, Sass, Javascript and JQuery.  This project is fully responsive and have a background stars with parallax effect animation impemented with sass functions and css keyframes, also have a JavaScript functionality to handle a type writting effect at the hero page,  animated knowledge time line with SVG icons  and a pop window that shows different information referent to any of my porjects.',
+    text: 'It is my personalized web, built on HTML, Sass, Javascript and JQuery.  This project is fully responsive and have a background stars with parallax effect animation impemented with sass functions and css keyframes, also have a JavaScript functionality to handle a type writting effect at the hero page,  animated knowledge time line with SVG icons  and a pop window that shows different information referent to any of my porjects.',
     url: '#home',
-    repo: 'https://github.com/Erick-Hdez/My-porfolio',
+    repo: 'https://github.com/Erick-Hdez/Portafolio',
     icons: `
       <i class="devicon-html5-plain-wordmark colored"></i>
       <i class="devicon-sass-original colored"></i>
@@ -205,8 +200,7 @@ const modals = {
     title: 'Professional Portoflio',
     img: 4,
     id: 'modal-7',
-    text:
-      'Professional portfolio web template fully responsive made with HTML Sass and JavasCript"',
+    text: 'Professional portfolio web template fully responsive made with HTML Sass and JavasCript"',
     url: 'https://wizardly-nobel-d3f330.netlify.app/',
     repo: 'https://github.com/Erick-Hdez/portfolio_website',
     icons: `
